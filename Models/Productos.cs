acd6904bce069dd73deb07a5aa55aa13e1dea52a
Robert <robert_gutierrez@usmp.pe>
2020-12-09 20:24:28 -0500

    casi terminado v2

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -8,18 +8,19 @@ namespace PC_03.Models
         
         
         public int id { get; set; }
+        [Required]
         public string nombre{ get; set; }
-
+        [Required]
         public string imagen { get; set; }
-
+        [Required]
         public string descripcion { get; set; }
-
+        [Required]
         public double precio { get; set; }
-
+        [Required]
         public string celular { get; set; }
-
+        [Required]
         public string lugarcompra { get; set; }
-
+        [Required]
         public string usuario { get; set; }
     }
 }
